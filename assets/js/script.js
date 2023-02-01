78ff1ae7324d7d4c7ab99eb835b814ea7d1e4ae3
jrob02180 <jrob02180@gmail.com>
2023-01-31 22:52:25 -0700

    added pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+// When I type a city in the input field and click 'search', the current weather for that city displays in the top right container
+// the weather for the next 5 days for that city displays in the bottom right container
+    // the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed are listed
+        // create vairable for each of the above
+            // var ? = document.createElement('h3')
+            // var ? = document.createElement('p')
+        // add text content= data[i].?
+        // userContainer.append(wind, etc.)
+        // if > then?
+        
+        // The previously searched cities display under the search button
+            // Add to local storage
+        
+// When clicking on a city in search history, current and future conditions display egain
+
+// Add event listener  
+    // searchButton.addEventListener('click', getApi);
